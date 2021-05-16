279f0e5d61e8403baa253dcaa2cb6e3d692dad51
Jesse Ayegba <jessejoseph64@gmail.com>
2021-05-16 12:36:20 +0100

    Initial Commit - React router setup in progress

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Dashboard.css";
+
+export default function Dashboard() {
+    return(
+        <div className="dashboard">
+            <h1>I am the dashboard</h1>
+        </div>
+    )
+}
---
+++ src/pages/UserDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./UserDetail.css";
+
+export default function UserDetail() {
+    return(
+        <div className="userDetail">
+            <h1>I a User's detail Component</h1>
+        </div>
+    )
+}
