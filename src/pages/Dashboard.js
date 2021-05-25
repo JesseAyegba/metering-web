3a0282b5d22431bd6a0a59d44f8cbd84c9aaf089
Jesse Ayegba <jessejoseph64@gmail.com>
2021-05-25 21:42:54 +0100

    Commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,11 +5,38 @@ import DoughnutChart from "../components/Doughnut";
 import Header from "../components/Header";
 import SideNav from "../components/SideNav";
 import { audioUploads, notifications, users } from "../data/cardsData";
+import { db } from "../firebase";
 import "./Dashboard.css";
 
 export default function Dashboard() {
+  const handleClick = async () => {
+  db.collection("users").doc("mosh@gmail.com")
+      .get()
+      .then((user) => {
+        if (user.exists) {
+          console.log(user);
+        } else {
+          console.log("User does not exist");
+        }
+      })
+      .catch((error) => {
+        console.log("Something went wrong", error);
+      });
+  };
   return (
     <div className="dashboard">
+      <button
+        onClick={() => handleClick()}
+        style={{
+          padding: 40,
+          margin: 10,
+          backgroundColor: "blue",
+          color: "white",
+        }}
+        className="login__btnTester"
+      >
+        Click me
+      </button>
       <div className="dashboard__sideNav">
         <SideNav />
       </div>
