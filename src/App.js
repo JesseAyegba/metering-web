aba13f315875d1d75e911c150452c02f0ed03dc1
Jesse Ayegba <jessejoseph64@gmail.com>
2021-06-26 18:20:42 +0100

    Routing refactored

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import UserDetail from "./pages/UserDetail";
 import Login from "./pages/Login";
@@ -15,17 +15,27 @@ export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={auth ? Dashboard : Landing} />
-        <Route exact path="/login/" component={auth ? Dashboard : Login} />
-        <Route exact path="/dashboard/" component={auth ? Dashboard : Login} />
-        <Route exact path="/users/" component={auth ? Users : Login} />
-        <Route
-          exact
-          path="/users/:userId/"
-          component={auth ? UserDetail : Login}
-        />
-        <Route exact path="/uploads/" component={auth ? Uploads : Login} />
-        <Route exact path="*" component={PageNotFound} />
+        <Route exact path="/">
+          {auth ? <Redirect to="/dashboard/" /> : <Landing />}
+        </Route>
+        <Route exact path="/login/">
+          {auth ? <Redirect to="/dashboard/" /> : <Login />}
+        </Route>
+        <Route exact path="/dashboard/">
+          {auth ? <Dashboard /> : <Redirect to="/login/" />}
+        </Route>
+        <Route exact path="/users/">
+          {auth ? <Users /> : <Redirect to="/login/" />}
+        </Route>
+        <Route exact path="/users/:userId/">
+          {auth ? <UserDetail /> : <Redirect to="/login/" />}
+        </Route>
+        <Route exact path="/uploads/">
+          {auth ? <Uploads /> : <Redirect to="/login/" />}
+        </Route>
+        <Route exact path="*">
+          <PageNotFound />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
